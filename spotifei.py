ef51a2d7b1f660c67e1ae22104a67d6df2913a57
Sophia Taddeo Nascimento <sophiataddeon@gmail.com>
2025-04-11 16:47:39 -0300

    projeto spotifei
    projeto spotifei para a materia fudamento de algoritmos

---
+++ spotifei.py
@@ -0,0 +1,31 @@
+#menu função
+#menu sec função
+#cadastre função
+#login função
+
+musica=[["Twice",'Kpop','What is love'],["Roupa Nova", 'Pagode', 'Whisky a go go ']] #lista com músicas
+
+
+print('Bem-Vindo ao SPOTFEI!')
+
+
+# info=int(input("Você deseja realizar o CADASTRO (1) ou o LOGIN(2) \n"))
+# for escolha in 'spotifei':
+#  if info == 1:
+#     lista_usuario=[]
+#     nome=str(input('Digite seu nome: \n'))
+#     user=str(input('Digite seu user: \n'))
+#     senha=int(input('Digite uma senha: \n'))
+#     lista_usuario.append(nome)
+#     lista_usuario.append(user)
+#     lista_usuario.append(senha)
+
+# if info == 2:
+#     if senha == lista_usuario:
+#         acao = int(input('O que você quer fazer agora? Digite 1 para buscar música, 2 para gerenciar playlist e 3 para visualizar histórico: \n'))
+
+
+
+
+# lista.append(lista_usuario)
+
